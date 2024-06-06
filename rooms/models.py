9890e4648e6ba47d6af3bfe16482cb2801d91442
kangjiungdev <kangjiungdev@gmail.com>
2024-06-07 03:14:15 +0900

    #6.4 Rooms Admin

--- rooms/models.py
+++ rooms/models.py
@@ -10,6 +10,7 @@ class Room(CommonModel):
         PRIVATE_ROOM = "private_room", "Private Room"
         SHARED_ROOM = "shared_room", "Shared Room"
 
+    name = models.CharField(max_length=180, default="")
     country = models.CharField(max_length=50, default="한국")
     city = models.CharField(max_length=80, default="서울")
     price = models.PositiveIntegerField()
@@ -18,9 +19,12 @@ class Room(CommonModel):
     description = models.TextField()
     address = models.CharField(max_length=250)
     pet_friendly = models.BooleanField(default=True)
-    kind = models.CharField(max_length=20, choices=RoomKindChoices.choices,)
+    kind = models.CharField(max_length=20, choices=RoomKindChoices.choices)
     owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
     amenities = models.ManyToManyField("rooms.Amenity")
+
+    def __str__(self) -> str:
+        return self.name
 
 class Amenity(CommonModel):
 
@@ -28,3 +32,9 @@ class Amenity(CommonModel):
 
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=150, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Amenities"
